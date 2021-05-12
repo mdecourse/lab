45c84092d98f3352420ce276d06f22016bb9ce90
mdecourse <mdecourse@gmail.com>
2021-05-12 09:22:27 +0800

    add FANUC M-710iC/50 images and info

---
+++ images/fanuc_m710ic_50_6_files/configuration.js
@@ -0,0 +1,51 @@
+window._automatic_tracking_config = {
+    samplingRate: {
+        '.ebay.com.sg': 100, // Singapore
+        '.ebay.ie': 100, // Ireland
+        '.ebay.es': 100, // Spain
+        '.ebay.co.uk': 100, // UK
+        '.ebay.de': 100, // German
+        '.ebay.com.au': 100, // Australia
+        '.ebay.com': 100, // US
+        '.ebay.fr': 100, // France
+        '.ebay.ca': 100, // Canada
+        '.ebay.at': 100, // Austria
+        '.befr.ebay.be': 100, // Belgium (French)
+        '.benl.ebay.be': 100, // Belgium (Dutch)
+        '.ebay.ch': 100, // Switzerland
+        '.ebay.com.hk': 100, // HongKong
+        '.ebay.in': 100, // India
+        '.ebay.it': 100, // Italy
+        '.ebay.com.my': 100, // Malaysia
+        '.ebay.nl': 100, // Netherlands
+        '.ebay.ph': 100, // Philippines
+        '.ebay.pl': 100, // Poland
+        '.cafr.ebay.ca': 100, // Canada (French)
+        '.ebay.be': 100,
+    },
+    moduleImpression: {
+        '.qa.ebay.com': true, // QA
+        '.ebay.com.sg': true, // Singapore
+        '.ebay.ie': false, // Ireland
+        '.ebay.es': false, // Spain
+        '.ebay.co.uk': false, // UK
+        '.ebay.de': false, // German
+        '.ebay.com.au': false, // Australia
+        '.ebay.com': false, // US
+        '.ebay.fr': false, // France
+        '.ebay.ca': false, // Canada
+        '.ebay.at': false, // Austria
+        '.befr.ebay.be': false, // Belgium (French)
+        '.benl.ebay.be': false, // Belgium (Dutch)
+        '.ebay.be': false, // Some edge pages for Belgium
+        '.ebay.ch': false, // Switzerland
+        '.ebay.com.hk': false, // HongKong
+        '.ebay.in': false, // India
+        '.ebay.it': false, // Italy
+        '.ebay.com.my': false, // Malaysia
+        '.ebay.nl': false, // Netherlands
+        '.ebay.ph': false, // Philippines
+        '.ebay.pl': false, // Poland
+        '.cafr.ebay.ca': false, // Canada (French)
+    },
+};
